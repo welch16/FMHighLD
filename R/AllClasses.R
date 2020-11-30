7841651e5e787066807bd237c06b363c2535c1c2
Rene Welch <welch16@gmail.com>
2020-11-29 22:14:51 -0600

    added FmIter class

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -4,11 +4,52 @@ NULL
 
 
 #' @rdname RandParam
-#' @export
 setClass("RandParam",
   representation = representation(strategy = "character"),
   prototype = prototype(strategy = "none"))
 
-setValidity("RandParam",
+
+
+#' @rdname FmIter
+#' @export
+#' @importClassesFrom Matrix Matrix
+#' @importFrom purrr map_chr
+setClass("FmIter",
+  representation = representation(
+    singletrait = "logical",
+    models = "list",
+    gamma = "Matrix",
+    mu = "Matrix",
+    sigma = "Matrix"),
+  prototype = prototype(
+    singletrait = TRUE,
+    models = list(),
+    gamma = Matrix::Matrix(nrow = 0, ncol = 0),
+    mu = Matrix::Matrix(nrow = 0, ncol = 0),
+    sigma = Matrix::Matrix(nrow = 0, ncol = 0)))
+
+setValidity("FmIter",
   function(object) {
-  })
+
+    if (length(object@models) == 0) {
+      out <- TRUE
+    } else {
+      out <- all(purrr::map_chr(object@models, class) == "lm") |
+        all(purrr::map_chr(object@models, class) == "lmerMod")
+    }
+    out
+
+  }
+)
+
+#' @name FmIter object
+#'
+#' \code{FmIter} contains the elements of an iteration for the FMHighLD
+#' algorithm.
+#'
+#' @aliases FmIter FmIter-class
+#'
+#' @docType class
+#' @rdname FmIter
+#'
+NULL
