def6dd0c8c19e76ae35eb5717ef78859277f72d2
Rene Welch <welch16@gmail.com>
2021-01-18 20:08:48 -0600

    added tests for kl and jsd

--- tests/testthat/test-jsd_works.R
+++ tests/testthat/test-jsd_works.R
@@ -4,3 +4,16 @@ test_that("jsd gets zero for same vector", {
   expect_equal(jsd(p1, p1), 0)
 
 })
+
+test_that("jsd works for two vectors", {
+
+  p1 <- 1:5
+  p1 <- p1 / sum(p1)
+  q1 <- c(4, 3, 2, 1, 4)
+  q1 <- q1 / sum(q1)
+
+  m1 <- colMeans(rbind(p1, q1))
+
+  expect_equivalent(jsd(p1, q1), mean(c(kl(p1, m1), kl(q1, m1))))
+
+})
--- tests/testthat/test-kl_works.R
+++ tests/testthat/test-kl_works.R
@@ -4,3 +4,14 @@ test_that("kl gets zero for same vector", {
 
   expect_equal(kl(p1, p1), 0)
 })
+
+test_that("kl works for two vectors", {
+
+  p1 <- 1:5
+  p1 <- p1 / sum(p1)
+  q1 <- c(4, 3, 2, 1, 4)
+  q1 <- q1 / sum(q1)
+
+  expect_equivalent(kl(p1, q1), sum(p1 * log(p1 / q1)))
+
+})
