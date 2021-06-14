f6fc6ed54da6ad3d740cd17a00e69a722ec09d33
Rene Welch <welch16@gmail.com>
2021-06-13 19:34:36 -0500

    get_ld_clusters tests done

--- tests/testthat/test-correlation-works.R
+++ tests/testthat/test-correlation-works.R
@@ -45,3 +45,66 @@ test_that("noise_cor error if eidim is not numeric", {
   expect_error(noise_cor(diag(rep(1, 10)), epsilon = 3, "av"))
 
 })
+
+test_that("sim_block_cor error when block_sizes and corrs have diff. sizes", {
+  block_sizes <- c(1, 2, 3)
+  corrs <- c(.3, .5)
+  expect_error(sim_block_cor(block_sizes, corrs))
+
+})
+
+test_that("sim_toeplitz_cor error when block_sizes and corrs have diff sizes", {
+  block_sizes <- c(1, 2, 3)
+  corrs <- c(.3, .5)
+  expect_error(sim_toeplitz_cor(block_sizes, corrs))
+
+})
+
+test_that("get_ld_clusters works", {
+
+  set.seed(12345)
+  nsnps <- 20
+  nldblocks <- 4
+
+  nsnps / nldblocks
+
+  snp_names <- stringr::str_c("snp", seq_len(nsnps))
+
+  ld_mat <- sim_block_cor(
+    block_sizes = rep(nsnps / nldblocks, nldblocks),
+    corrs = rep(.8, nldblocks),
+    delta = .2,
+    epsilon = .1,
+    eidim = 2)
+
+  rownames(ld_mat) <- colnames(ld_mat) <- snp_names
+
+  expect_equal(length(table(get_ld_clusters(ld_matrix = ld_mat, 0.7))),
+    nldblocks)
+  expect_true(all(
+    table(get_ld_clusters(ld_matrix = ld_mat, 0.7)) == nsnps / nldblocks))
+
+})
+
+test_that("get_ld_clusters error when min_r2 is not correlation", {
+
+  set.seed(12345)
+  nsnps <- 20
+  nldblocks <- 4
+
+  nsnps / nldblocks
+
+  snp_names <- stringr::str_c("snp", seq_len(nsnps))
+
+  ld_mat <- sim_block_cor(
+    block_sizes = rep(nsnps / nldblocks, nldblocks),
+    corrs = rep(.8, nldblocks),
+    delta = .2,
+    epsilon = .1,
+    eidim = 2)
+
+  rownames(ld_mat) <- colnames(ld_mat) <- snp_names
+
+  expect_error(get_ld_clusters(ld_mat, min_r2 = 3))
+  expect_error(get_ld_clusters(ld_mat, min_r2 = -1))
+})
